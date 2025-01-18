adc5bc90913cf0681e1beebd11f726ae47708d89
Elias Borda <elias@androbin.co>
2025-01-17 23:40:19 -0300

    WIP: CharacterMovement component, Added procedural texture to ground

--- components/CharacterMovement.gd
+++ components/CharacterMovement.gd
@@ -2,16 +2,15 @@ class_name CharacterMovement
 extends Node
 
 
-
 @onready var parent: CharacterBody3D = get_parent()
 
-@export var acceleration: float = 10.0
+@export var acceleration: float = 24.0
 
-@export var max_speed: float = 200.0
+@export var max_speed: float = 320.0
 
-@export var damping: float = 0.2
+@export var damping: float = 48.0
 
-@export var jumping_force: float = 200.0
+@export var jumping_force: float = 240.0
 
 
 var vel: Vector3 = Vector3.ZERO
@@ -21,8 +20,31 @@ func aim(direction: Vector2) -> void:
 	pass
 
 func move(direction: Vector2) -> void:
-	vel.x += direction.x * acceleration
+	if (
+		direction.length() == 0.0 
+		and abs(vel.x) > 0
+	):
+		# Movement damping
+		if parent.is_on_floor():
+			vel.x -= sign(vel.x) * damping
+		else:
+			# Less friction on air
+			vel.x -= sign(vel.x) * (damping * 0.1)
+		# Prevent drifting
+		if abs(vel.x) <= damping:
+			vel.x = 0
+	
+	else:
+		# Accelerates
+		if abs(vel.x) < max_speed:
+			if sign(direction.x) <= 0:
+				vel.x += direction.x * acceleration
+			else:
+				# Suddenly changes direction
+				vel.x += direction.x * acceleration * 2
+	
 	clamp(vel.x, -max_speed, max_speed)
+
 
 func jump() -> void:
 	if parent.is_on_floor():
@@ -36,6 +58,7 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	# Gravity
 	if parent.is_on_floor() and vel.y < 0.0:
 		vel = Vector3(vel.x, 0, vel.z)
 	else:
